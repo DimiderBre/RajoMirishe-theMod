0705804ff507526c63ac390ac7c09776a59e46a3
DimiderBre <dimitar.stanev.e.u@gmail.com>
2025-11-02 13:37:01 +0100

    Something something github not wanting to pull my things.

---
+++ src/main/java/rajo/mirishe/RajoMirisheDataGenerator.java
@@ -0,0 +1,11 @@
+package rajo.mirishe;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RajoMirisheDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
